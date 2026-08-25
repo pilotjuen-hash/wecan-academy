9d35b1e4a03f87b8a64be1227c2c845260dfae6b
JohnMwangikenya <johnmwangi541@gmail.com>
2026-08-25 13:56:53 +0300

    added-react-LoginForm-LoginPage-linktofilesin-app.jsx

---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,60 @@
+
+function LoginForm(){
+  return (
+ <>
+  {/* Right Section */}
+  <section className="form-section">
+    <div className="login-container">
+      <div className="mobile-logo">WCA</div>
+      <h1>WECAN-ACADEMY</h1>
+      <div className="heading">
+        <h2>Sign in</h2>
+        <p>Sign in to access your WECAN-ACADEMY community account</p>
+      </div>
+      <form>
+        <div className="form-group">
+          <label htmlFor="email">Email or Phone Number</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            placeholder="email@example.com or +254..."
+            required=""
+          />
+        </div>
+        <div className="form-group">
+          <div className="password-label">
+            <label htmlFor="password">Password</label>
+            <a href="#">Forgot password?</a>
+          </div>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Enter your password"
+            required=""
+          />
+        </div>
+        <div className="remember">
+          <label>
+            <input type="checkbox" name="remember" />
+            <span>Remember me</span>
+          </label>
+        </div>
+        <button type="submit">Sign in</button>
+      </form>
+      <p className="signup">
+        Don't have an account? <a href="register..html">Sign up</a>
+      </p>
+      <a href="#" className="back-home">
+        ← Back to Home
+      </a>
+    </div>
+  </section>
+</>
+
+  );
+}
+
+
+export default LoginForm;
